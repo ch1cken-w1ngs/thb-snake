6cc350c952c4c01b4d699364066a2b74eeb83c16
ch1cken-w1ngs <84854571+ch1cken-w1ngs@users.noreply.github.com>
2022-06-03 10:30:19 +0200

    script in eigene File gequetscht

---
+++ main.js
@@ -0,0 +1,76 @@
+
+var playerlength = 1;
+let snake = []
+
+kaboom();
+loadSprite("frodo", "Frodo.jpg")  
+for(let i = 1; i <= playerlength; i++){
+    let segment = add([
+    health(1),
+    sprite("frodo"),
+    area(),
+    pos(100, 100),
+    "snake"
+    ])
+    snake.push(segment)
+    }
+
+onKeyPress("up",() => {
+    // Hier passiert jetzt etwas wie zum Beispiel:
+    player.move(0,-10)
+})
+onKeyPress("left",() => {
+    // Hier passiert jetzt etwas wie zum Beispiel:
+    player.move(0,-10)
+})
+onKeyPress("down",() => {
+    // Hier passiert jetzt etwas wie zum Beispiel:
+    player.move(0,-10)
+})
+onKeyPress("right",() => {
+    // Hier passiert jetzt etwas wie zum Beispiel:
+    player.move(0,-10)
+})
+
+onCollide("player","border", (border) => {
+    destroy(player)
+    go("lose")
+})
+onCollide("player", "fruit", (fruit) => {
+    playerlength++
+})
+let fruit = add([
+    color(0, 255, 0),
+    rect(25, 25),
+    pos(),
+])
+
+            
+    addLevel([
+    "===============",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "=             =",
+    "==============="
+
+], {
+    width: 38,
+    height: 38,
+    
+    "=": () =>[
+    rect(50,50),
+    color(255,0,0),
+    area(),
+    "border"
+    ],
+})
